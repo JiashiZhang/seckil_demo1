33081a6a620eadd36e3abfccef4a5724b8e92b50
JiashiZhang <jszhang710@gmail.com>
2022-02-28 21:14:59 -0600

    Initial commit

---
+++ src/main/java/com/example/seckil_demo1/SeckilDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.seckil_demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckilDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckilDemo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seckil_demo1/SeckilDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seckil_demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckilDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
